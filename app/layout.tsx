f89c4a5e6d635924f598a443447cae9ae56fde34
Alonso <alonso.hl25@gmail.com>
2026-03-01 23:16:45 -0800

    update vercel analitycs

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import localFont from "next/font/local";
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/next";
 import "./globals.css";
 import { Sidebar } from "@/src/components/navigation/Sidebar";
 
@@ -40,6 +41,7 @@ export default function RootLayout({
       >
         {children}
         <SpeedInsights />
+        <Analytics />
       </body>
     </html>
   );
